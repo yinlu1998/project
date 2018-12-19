9b4c800cb4a9490245a4750fea4ced9915141e68
yinlu1998 <1981183186@qq.com>
2018-12-19 15:58:46 +0800

    FreeMarker

--- FreeMarker/WebTestListener.java
+++ FreeMarker/WebTestListener.java
@@ -1,78 +1,133 @@
-package com.webtest.core;
+package com.freemarker;
 
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import org.openqa.selenium.WebDriver;
 import org.testng.ITestContext;
+import org.testng.ITestNGMethod;
 import org.testng.ITestResult;
 import org.testng.TestListenerAdapter;
-import org.testng.annotations.Test;
 
+import com.webtest.core.BaseTest;
 import com.webtest.utils.Log;
-import com.freemark.FreeMarker;
-/**
- * author:lihuanzhen
- * 监听器，用例失败，截屏
- */
+
+public class WebTestListener extends TestListenerAdapter {
 
 
-public class WebTestListener  extends TestListenerAdapter{
-	  @Override
-	  public void onTestFailure(ITestResult tr) {
-		  
-		  Log.error(tr.getInstance()+"-"+tr.getName()+"运行失败，需要截屏");
-		  BaseTest tb = (BaseTest) tr.getInstance();
-          WebDriver driver = tb.getDriver();    
-		  SeleniumScreenShot ss = new SeleniumScreenShot(driver);
-		  ss.screenShot();
-		  
-	  }
-	  
-	  
-	  	@Test
-		@Override
-		public void onFinish(ITestContext testContext) {
-			// TODO 自动生成的方法存根
-			super.onFinish(testContext);
-			
-			
-			Map<String, Object> dataMap =new HashMap<String, Object>();
-			dataMap.put("stuname","chanzhi");
+	FreemarkerTemplateEngine ft=new FreemarkerTemplateEngine();
 
-			
-			List<ITestResult> fail=this.getFailedTests();
-			List<ITestResult> success=this.getPassedTests();
-			
-			System.out.println("测试结果如下：");
-			System.out.println("失败的测试用例"+fail);
-			System.out.println("成功的测试用例"+success);
-			
-			
-			dataMap.put("slist1", fail);
-			dataMap.put("slist",success);
-			try {
-				FreeMarker.freemarker("src/com/freemark/templates","src/com/freemark","report.ftl",dataMap);
-			} catch (Exception e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}	
-			
-//			Map<ITestResult,List> mapfail = new HashMap<>();
-//		        for (ITestResult list : fail) {
-//		            mapfail.put(list, fail);
-//		        }
-//		        System.out.println("map失败"+mapfail);
-//		        
-//		    
-//		    Map<ITestResult,List> mapsuccess = new HashMap<>();
-//		        for (ITestResult list : success) {
-//		            mapsuccess.put(list, success);
-//		        }
-//		        System.out.println("map成功"+mapsuccess);
-		        
-		    
-		    
+	public WebTestListener() {
+		super();
+	}
+	
+	private String writeResultToMailTemplate()
+	{
+		ITestNGMethod method[]=this.getAllTestMethods();
+		List failedList=this.getFailedTests();
+		List passedList=this.getPassedTests();
+		
+		
+		List failedList1=new ArrayList();
+		List passedList1=new ArrayList();
+		for(int j=0;j<failedList.size();j++)
+		{
+			ITestResult tr=(ITestResult) failedList.get(j);
+			for(int i=0;i<method.length;i++)
+			{
+				if(tr.getMethod().getMethodName().equals(method[i].getMethodName()))
+				{
+					if(method[i].getDescription()!=null)
+					{
+						tr.setAttribute("name", method[i].getDescription());
+					}
+					else
+					{
+						tr.setAttribute("name", "");
+					}
+					break;
+				}
+			}
+			failedList1.add(tr);
 		}
+		for(int j=0;j<passedList.size();j++)
+		{
+			ITestResult tr=(ITestResult) passedList.get(j);
+			for(int i=0;i<method.length;i++)
+			{
+				if(tr.getMethod().getMethodName().equals(method[i].getMethodName()))
+				{
+					if(method[i].getDescription()!=null)
+					{
+						tr.setAttribute("name", method[i].getDescription());
+					}
+					else
+					{
+						tr.setAttribute("name", "");
+					}
+					break;
+				}
+			}
+			passedList1.add(tr);
+		}
+		Map context=new HashMap();
+    	context.put("date", new Date());
+        context.put("failedList",failedList);   
+        context.put("passedList",passedList1); 
+        context.put("casesize",passedList.size()+failedList.size()); 
+        context.put("failcasesize",failedList.size());
+        try {
+			String content=ft.run(context);
+			return content;
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	
+	@Override
+	public void onFinish(ITestContext testContext) {
+		// TODO Auto-generated method stub
+		super.onFinish(testContext);
+		//
+		if(System.getProperty("os.name").contains("dow"))
+		{
+			//return;
+		}
+		if(BaseTest.getCfgProperty("enable_email").equals("true"))
+		{
+			String emailContent=this.writeResultToMailTemplate();
+			String emailTitle=BaseTest.getCfgProperty("mail_title")+"----"+this.getTime();
+			String toMail=BaseTest.getCfgProperty("to_mail");
+			try {
+				if(this.getFailedTests()!=null&&this.getFailedTests().size()>0)
+				{
+					MailUtil.sendEmail(toMail,emailTitle, emailContent);
+					Log.info("email send to "+toMail+" success");
+				}else
+				{
+					MailUtil.sendEmail(BaseTest.getCfgProperty("success_to_mail"),emailTitle, emailContent);
+					Log.info("email send to "+BaseTest.getCfgProperty("success_to_mail")+" success");
+				}
+			} catch (UnsupportedEncodingException e) {
+				// TODO Auto-generated catch block
+				Log.fatal("email send fail :"+e.getMessage());
+			}
+		}
+	
+	}
+	
+
+
+    public String getTime()
+    {
+    	java.util.Calendar c=java.util.Calendar.getInstance();    
+        java.text.SimpleDateFormat f=new java.text.SimpleDateFormat("yyyy-MM-dd  hh:mm:ss");    
+       	return  f.format(c.getTime());    
+    }
 }
